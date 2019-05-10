135f07a2cbd8cdfbfd1f96be1ffd1fca09d254f9
Benjamin Fransson <benjamin@cafcon.se>
2019-05-10 18:55:35 +0200

    Removed http and implemented node-fetch.

--- services/yearsService.js
+++ services/yearsService.js
@@ -1,9 +1,22 @@
 // const puppeteer = require('puppeteer');
-const http = require('http');
+const fetch = require('node-fetch');
 const logger = require('../logger');
 
 const yearsService = async (req, res) => {
-  res.status(200).json('test');
+  const url = 'https://www.skatteverket.se/st-api/rest/v1/kommuner';
+  let years;
+
+  await fetch(url)
+    .then(response => response.json())
+    .then((json) => {
+      years = {
+        years: Object.keys(json)
+      };
+
+      logger.debug(json);
+
+      res.json(years);
+    });
 };
 
 module.exports = yearsService;
