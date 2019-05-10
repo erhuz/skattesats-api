bc7374ba96aeb0f3dcfd8982b643d98e365a4d8d
Benjamin Fransson <benjamin@cafcon.se>
2019-05-10 19:51:43 +0200

    Implemented municipalitiesService

---
+++ services/municipalitiesService.js
@@ -0,0 +1,29 @@
+// const puppeteer = require('puppeteer');
+const fetch = require('node-fetch');
+const logger = require('../logger');
+
+const municipalitiesService = async (req, res) => {
+  const url = 'https://www.skatteverket.se/st-api/rest/v1/kommuner';
+  const { year } = req.params;
+
+  await fetch(url)
+    .then(response => response.json())
+    .then((json) => {
+      const output = {
+        municipalities: json[year]
+      };
+
+      console.log(output);
+
+      // output.municipalities.forEach((name, index) => {
+      //   output.municipalities[index] = name
+      //     .replace('å', 'a')
+      //     .replace('ä', 'a')
+      //     .replace('ö', 'o');
+      // });
+
+      res.json(output);
+    });
+};
+
+module.exports = municipalitiesService;
