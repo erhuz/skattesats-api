08946e9c86c89e6bbb723742a42997b7da910af1
Benjamin Fransson <benjamin@cafcon.se>
2019-05-10 18:23:15 +0200

    Service now uses http module instead of puppeteer

--- services/yearsService.js
+++ services/yearsService.js
@@ -1,15 +1,9 @@
-const puppeteer = require('puppeteer');
+// const puppeteer = require('puppeteer');
+const http = require('http');
 const logger = require('../logger');
 
 const yearsService = async (req, res) => {
-  const browser = await puppeteer.launch({
-    headless: false
-  });
-  const page = await browser.newPage();
-
-  await page.goto('https://www.skatteverket.se/privat/skatter/arbeteochinkomst/skattetabeller.4.18e1b10334ebe8bc80005221.html');
-
-  res.status(200).json({});
+  res.status(200).json('test');
 };
 
 module.exports = yearsService;
